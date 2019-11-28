8736875ca3fe25e2fc7e783d08edf26155a6556a
akinayap <akina.yap@gmail.com>
2019-11-28 23:25:54 +0800

    SMS will keep sending when reach threshold (Must change)

---
+++ PolarHeartRateApplication-edited/app/src/main/java/org/marco45/polarheartmonitor/AboutActivity.java
@@ -0,0 +1,17 @@
+package org.marco45.polarheartmonitor;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * About activy displaing info about the app
+ * @author marco
+ *
+ */
+public class AboutActivity extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+	}
+}
